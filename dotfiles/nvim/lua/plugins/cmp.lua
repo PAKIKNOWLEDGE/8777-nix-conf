ae9e4135a89f5181409cad7d27cae1afca148075
PAKI KNOWLEDGE <2772962906@qq.com>
2026-06-20 18:55:16 +0800

    nvim conf

--- dotfiles/nvim/lua/plugins/cmp.lua
+++ dotfiles/nvim/lua/plugins/cmp.lua
@@ -4,6 +4,7 @@ return {
     -- VS Code 风格：自动弹出但不自动选中，回车不会误触
     opts.completion = opts.completion or {}
     opts.completion.completeopt = "menu,menuone,noselect"
+    opts.keyword_length = 3
     opts.preselect = require("cmp.types.cmp").PreselectMode.None
     return opts
   end,
