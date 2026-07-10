4ebe57053eff612f791145c87a8221b7572d3f8e
PAKI KNOWLEDGE <2772962906@qq.com>
2026-07-10 18:55:29 +0800

    added 'new device' 占位

--- hosts/NewPC/default.nix
+++ hosts/NewPC/default.nix
@@ -1,3 +1,11 @@
 { ... }: {
   networking.hostName = "NewPC";
+
+  # 占位：等新机器到了 nixos-generate-config 生成后替换
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/CHANGE-ME";
+    fsType = "ext4";
+  };
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 }
