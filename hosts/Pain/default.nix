51e56c60c12c6d7516dcc90f24973ab4d9c55ef4
PAKI KNOWLEDGE <2772962906@qq.com>
2026-07-17 00:54:47 +0800

    added 3pkg for host-Pain:zed/nil/nixd ; change 2 starship config

--- hosts/Pain/default.nix
+++ hosts/Pain/default.nix
@@ -1,12 +1,12 @@
 { config, pkgs, ... }: {
   networking.hostName = "PainkyCrush";
   imports = [ ./hardware.nix ];
-
-  # ── Pain 专属软件包 ──
-  #   这里只放这台机器独有的包，公共包在 configuration.nix 管理
   environment.systemPackages = with pkgs; [
     macchina                           # hyfetch用的
-
+    zed-editor
+    nil # for zed's LSP
+    nixd # 同上
   ];
 
+  
 }
