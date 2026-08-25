e08a7d29dea1092492b2ded2a6ccdfab5f70c416
PAKI KNOWLEDGE <2772962906@qq.com>
2026-08-26 02:48:14 +0800

    added alias cl = 'clear' in fish

--- dotfiles/common/fish/config.fish
+++ dotfiles/common/fish/config.fish
@@ -41,6 +41,7 @@ alias rebuild='sudo nixos-rebuild switch'
 alias avim='nvim' # 肌肉记忆没救了
 alias homebuild='home-manager switch'
 alias ship='nix-shell -p updog --run "updog -d $(pwd)"'
+alias cl='clear'
 
 # yazi 退出后自动 cd 到浏览目录
 function y
